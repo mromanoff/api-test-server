630ddb73f40f690da9bb6cfc17294338d3b5a708
Michael Romanoff <zelcie@gmail.com>
2016-10-05 15:56:55 -0700

    update highlights

--- app.js
+++ app.js
@@ -21,26 +21,54 @@ mongoose.connect('mongodb://localhost/romanoff-io', function (err) {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+//app.use(cookieParser());
 
-app.set('trust proxy', 1); // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+// app.set('trust proxy', 1); // trust first proxy
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { path: '/', httpOnly: false, secure: false, maxAge: null }
+// }));
+
 
 var corsOptions = {
-  //origin: 'http://localhost:4000',
-  origin: true,
+  origin: 'http://localhost:4000',
   methods: ['GET', 'PUT', 'POST'],
   credentials: true,
+  //preflightContinue: true,
+ // allowedHeaders: 'Content-Type, credentials',
+  maxAge: 600,
+  //exposedHeaders: 'Access-Control-Allow-Origin',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors(corsOptions));
+
+
+var sessionOptions = {
+  secret: 'keyboard cat',
+  resave: false,
+  //name: 'romanoff.io',
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: false,
+    secure: false,
+    path:'/',
+    //maxAge: null
+    //,
+    //domain: 'localhost:4000'
+  }
+};
+
+// if (app.get('env') === 'production') {
+//   app.set('trust proxy', 1); // trust first proxy
+//   sess.cookie.secure = true; // serve secure cookies
+// }
+
+app.use(session(sessionOptions));
+
 
 // mount all routes on /api path
 app.use('/api', routes);
